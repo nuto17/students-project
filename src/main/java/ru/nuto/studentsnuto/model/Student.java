547d94d70516b5192b15060d9cbdb9c60df265d1
nuto17 <nutolfg@gmail.com>
2025-02-06 08:21:44 +0300

    ??

---
+++ src/main/java/ru/nuto/studentsnuto/model/Student.java
@@ -0,0 +1,24 @@
+package ru.nuto.studentsnuto.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    @Column(unique = true)
+    private String email;
+    @Transient
+    private int age;
+
+}
